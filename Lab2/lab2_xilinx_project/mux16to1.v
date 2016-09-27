9c7711a166523e0d28f7c06a339fcd1f4c52e522
George Zhang <gzhang01@college.harvard.edu>
2016-09-27 19:01:24 -0400

    SLT started

--- Lab2/lab2_xilinx_project/mux16to1.v
+++ Lab2/lab2_xilinx_project/mux16to1.v
@@ -18,17 +18,18 @@ module mux16to1(X, Y, Z, op_code, overflow);
 	input wire [(N-1):0] X, Y;
 	input wire [3:0] op_code;
 	output wire [(N-1):0] Z;
+	wire overflow_add, overflow_sub;
 	output wire overflow;
 	wire [(N-1):0] and_out, or_out, xor_out, nor_out, add_out, sub_out, slt_out, srl_out, sll_out, sra_out;
 	
 
 	//YOUR CODE HERE - remember to use a separate file for each module you create
-	adder ADD (.X(X), .Y(Y), .Cin(1'b0), .Cout(overflow), .Z(add_out));
 	bitwise_nor NOR (.X(X), .Y(Y), .Z(nor_out));
 	bitwise_xor XOR (.X(X), .Y(Y), .Z(xor_out));
 	bitwise_or OR (.X(X), .Y(Y), .Z(or_out));
 	bitwise_and AND (.X(X), .Y(Y), .Z(and_out));
-	subtractor SUB (.X(X), .Y(Y), .Cin(1'b0), .Cout(overflow), .Z(sub_out));
+	adder ADD (.X(X), .Y(Y), .Cin(1'b0), .Cout(overflow_add), .Z(add_out));
+	subtractor SUB (.X(X), .Y(Y), .Cin(1'b0), .Cout(overflow_sub), .Z(sub_out));
 	
 	
 	
@@ -41,8 +42,9 @@ module mux16to1(X, Y, Z, op_code, overflow);
 	                                                  : (op_code[0] ? add_out : 0))
 								               : (op_code[1] ? (op_code[0] ? nor_out : xor_out)
 	                                                  : (op_code[0] ? or_out : and_out)));
-			
-			
+																	  
+	assign overflow = ~(|(op_code ^ 4'b0101)) ? overflow_add :
+							~(|(op_code ^ 4'b0110)) ? overflow_sub : 0;
 
 endmodule
 `default_nettype wire //some Xilinx IP requires that the default_nettype be set to wire
