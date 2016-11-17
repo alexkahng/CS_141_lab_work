0f8ca411d88d1b6b5034ea83900f33c829029946
George Zhang <gzhang01@college.harvard.edu>
2016-11-17 17:27:33 -0500

    writeup and tests complete

--- lab4/lab4/tests/j.asm
+++ lab4/lab4/tests/j.asm
@@ -1,5 +1,4 @@
-	xor $t0 $t0 $t0				# Zero out $t0
-	addi $t0 $t0 2				# Set $t0 to 2
+	addi $t0 $zero 2			# Set $t0 to 2
 	j A 						# Jumps to label A
 	addi $t1 $t0 4				# Should NOT set $t1 to 6!
 A: 	addi $t2 $t0 2				# Sets $t2 to 4
--- lab4/lab4/tests/sll.asm
+++ lab4/lab4/tests/sll.asm
@@ -1,4 +1,3 @@
-addi $t0 $zero 1		# Set $t0 = 1
-sll  $t0 $t0 3			# Set $t0 = 8
-addi $t0 $t0 1			# Set $t0 = 9
-sll $t0 $t0 2			# Set $t0 = 36
+nor $t0 $zero $zero		# Set $t0 = -1
+sll $t0 $t0 3			# Set $t0 = -8
+sll $t0 $t0 2			# Set $t0 = -32
